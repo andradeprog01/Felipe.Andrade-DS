efb344818df116625014654bd7fa0ea8dc3fe27d
andradeprog01 <felipeandrade13012010@gmail.com>
2026-04-10 09:18:54 -0300

    Add files via upload

--- Main3.java
+++ Main3.java
@@ -1,34 +1,34 @@
 import java.util.Scanner;
-
-public class Main3 {
-    public static void main(String[] args) {
-        Scanner sc1 = new Scanner(System.in);
-        
-        int tentativa = 0;
-        String login = "felipe";
-        int senha = 1234;
-        
-        
-        while (tentativa < 3) {
-            System.out.println("\nTentativa " + (tentativa + 1) + " de 3");
-            System.out.print("Entre com seu login:(nome usuario) ");
-            String login2 = sc1.next();
-            System.out.print("Entre com sua senha:(numeros) ");
-            int senha2 = sc1.nextInt();
-            
-            
-            if (login.equals(login2) && senha == senha2) {
-                System.out.println("Seja bem vindo!!");
-                break; 
-            } else {
-                tentativa++; 
-                System.out.println("Usuário ou senha incorretos!");
-                
-                if (tentativa == 3) {
-                    System.out.println("Acesso bloqueado!");
+public class Main3
+{
+	public static void main(String[] args) {
+	 Scanner sc1 = new Scanner(System.in);
+	
+	    //3) Sistema de Login com 3 Tentativas (while)
+	    
+	    int tentativa = 0;
+	    
+	    while (tentativa<3)
+	    {
+	        tentativa++;
+	            System.out.println("Número de tentativas ["+tentativa+"]");
+	            System.out.println("Entre com seu login: ");
+	            String lo = sc1.next();
+	            System.out.println("Entre com sua senha: ");
+	            int senha = sc1.nextInt();
+	
+                if (lo.equals("Felipe") && senha==1234)
+                {
+                    System.out.println("Seja bem vindo! login efetuado com sucesso!");
+                    break;
                 }
-            }
-        }
-        sc1.close();
-    }
-}
+                else if (tentativa==3)
+                {
+                    System.out.println("Voçê alcançou o maxímo de tentativas!");
+                }
+            
+	        }
+	    }
+	    
+	}
+
